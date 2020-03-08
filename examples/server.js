1a3bb13e1c43a54c06c29b728022b3b837669d04
huanghanzeng <3505563308@qq.com>
2020-03-08 15:29:23 +0800

    初始化

---
+++ examples/server.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const webpack = require('webpack')
+const webpackDevMiddleware = require('webpack-dev-middleware')
+const webpackHotMiddleware = require('webpack-hot-middleware')
+const WebpackConfig = require('./webpack.config')
+
+const app = express()
+const compiler = webpack(WebpackConfig)
+
+app.use(webpackDevMiddleware(compiler, {
+  publicPath: '/__build__/',
+  stats: {
+    colors: true,
+    chunks: false
+  }
+}))
+
+app.use(webpackHotMiddleware((compiler)))
+app.use(express.static(__dirname))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extend: true}))
+
+const router = express.Router()
+
+router.get('/simple/get', function(req, res) {
+  res.json({
+    msg: 'Hello world'
+  })
+})
+
+app.use(router)
+
+const port = process.env.PORT || 3000
+
+module.exports = app.listen(port, () => {
+  console.log(`Edison chen is listening on http://localhost:${port}`)
+})
+
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { AxiosRequestConfig } from './types'
+import xhr from './xhr'
+function axios(config: AxiosRequestConfig) {
+  xhr(config)
+}
+
+export default axios
