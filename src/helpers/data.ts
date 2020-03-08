89cffc7d697aed0cdd6efd2aad8fe49ef4b2281e
huanghanzeng <3505563308@qq.com>
2020-03-08 19:56:47 +0800

    feat: 处理返回data

--- src/helpers/data.ts
+++ src/helpers/data.ts
@@ -6,3 +6,12 @@ export function transformRequest(data: any): any {
   }
   return data
 }
+
+export function transformResponse(data: any): any {
+  if (typeof data === 'string') {
+    try {
+      data = JSON.parse(data)
+    } catch (e) {}
+  }
+  return data
+}
--- src/xhr.ts
+++ src/xhr.ts
@@ -1,4 +1,5 @@
 import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
+import { parseHeaders } from './helpers/headers'
 
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
   return new Promise(resolve => {
@@ -18,7 +19,7 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
         data: responseData,
         status: request.status,
         statusText: request.statusText,
-        headers: responseHeaders,
+        headers: parseHeaders(responseHeaders),
         config,
         request
       }
