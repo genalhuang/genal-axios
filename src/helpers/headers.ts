3bfe0f68171426beb7e238eabd172022aae7c58b
huanghanzeng <3505563308@qq.com>
2020-03-08 17:51:49 +0800

    feat: 实现请求头处理

---
+++ src/helpers/headers.ts
@@ -0,0 +1,23 @@
+import { isPlainObject } from './util'
+
+function normalLizeHeaderName(headers: any, normalizeName: string): void {
+  if (!headers) {
+    return
+  }
+  Object.keys(headers).forEach(name => {
+    if (name !== normalizeName && name.toUpperCase() === normalizeName.toUpperCase()) {
+      headers[normalizeName] = headers[name]
+      delete headers[name]
+    }
+  })
+}
+
+export function processHeaders(headers: any, data: any): any {
+  normalLizeHeaderName(headers, 'Content-Type')
+  if (isPlainObject(data)) {
+    if (headers && !headers['Content-Type']) {
+      headers['Content-Type'] = 'application/json;charset=utf-8'
+    }
+  }
+  return headers
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,14 +1,23 @@
-export type Method = 'get' | 'GET'
-  | 'delete' | 'Delete'
-  | 'head' | 'HEAD'
-  | 'options' | 'OPTIONS'
-  | 'post' | 'POST'
-  | 'put' | 'PUT'
-  | 'patch' | 'PATCH'
+export type Method =
+  | 'get'
+  | 'GET'
+  | 'delete'
+  | 'Delete'
+  | 'head'
+  | 'HEAD'
+  | 'options'
+  | 'OPTIONS'
+  | 'post'
+  | 'POST'
+  | 'put'
+  | 'PUT'
+  | 'patch'
+  | 'PATCH'
 
 export interface AxiosRequestConfig {
   url: string
   method?: string
   data?: any
   params?: any
+  headers?: any
 }
