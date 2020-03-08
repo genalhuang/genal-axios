f0c88bbc7662aced058ace6bf56a64db66c1dd34
huanghanzeng <3505563308@qq.com>
2020-03-08 15:37:22 +0800

    第一个example

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -29,6 +29,17 @@ module.exports = {
           }
         ]
       },
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true
+            }
+          }
+        ]
+      },
     ]
   },
 
